6579061e4ee040273762eaa8f4d5ef479621b07f
bboygun <hegq1123@gmail.com>
2024-12-03 19:37:03 +0800

    修改题目描述

--- src/algo/leetcode/sliding_window_maximum.rs
+++ src/algo/leetcode/sliding_window_maximum.rs
@@ -20,8 +20,8 @@
 // 输出：[1]
 
 // 提示：
-// 1 <= nums.length <= 105
-// -104 <= nums[i] <= 104
+// 1 <= nums.length <= 10^5
+// -10^4 <= nums[i] <= 10^4
 // 1 <= k <= nums.length
 use crate::ds::heap::MaxHeap;
 
